af38b8d3c658378242af8b0d05f1f56e6961413e
Sportsdawg1 <league@WorthyMini-6.attlocal.net>
2018-08-26 15:55:57 -0700

    calculator, TweetFinder(ip)

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,21 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Calculator {
+public class Calculator implements ActionListener {
 	public int int1;
 	public int int2;
-	JLabel answer = new JLabel();
-	JTextField text1 = new JTextField();
-	JTextField text2 = new JTextField();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	int answerNum;
+	JLabel answer = new JLabel("hi");
+	JTextField text1 = new JTextField(10);
+	JTextField text2 = new JTextField(10);
 	JButton add = new JButton();
 	JButton sub = new JButton();
 	JButton mult = new JButton();
@@ -25,9 +33,51 @@ public class Calculator {
 		return int1 / int2;
 	}
 	void createUI () {
+		frame.setVisible(true);
+		text1.setEditable(true);
+		text2.setEditable(true);
 		add.setText("add");
 		sub.setText("subtract");
 		mult.setText("multiply");
 		div.setText("divide");
+		add.addActionListener(this);
+		sub.addActionListener(this);
+		mult.addActionListener(this);
+		div.addActionListener(this);
+		panel.add(text1);
+		panel.add(text2);
+		panel.add(add);
+		panel.add(sub);
+		panel.add(mult);
+		panel.add(div);
+		panel.add(answer);
+		frame.add(panel);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton buttonPressed = (JButton) e.getSource();
+		if (buttonPressed == add) {
+			int1 = Integer.parseInt(text1.getText());
+			int2 = Integer.parseInt(text2.getText());
+			answerNum = int1 + int2;
+			answer.setText(answerNum + "");
+		} else if (buttonPressed == sub) {
+			int1 = Integer.parseInt(text1.getText());
+			int2 = Integer.parseInt(text2.getText());
+			answerNum = int1 - int2;
+			answer.setText(answerNum + "");
+		} else if (buttonPressed == mult) {
+			int1 = Integer.parseInt(text1.getText());
+			int2 = Integer.parseInt(text2.getText());
+			answerNum = int1 * int2;
+			answer.setText(answerNum + "");
+		} else if (buttonPressed == div) {
+			int1 = Integer.parseInt(text1.getText());
+			int2 = Integer.parseInt(text2.getText());
+			answerNum = int1 / int2;
+			answer.setText(answerNum + "");
+		}
 	}
 }
--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -3,13 +3,8 @@ public class CalculatorRunner {
 
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
-		calculator.int1 = Integer.parseInt(JOptionPane.showInputDialog("int1"));
-		calculator.int2 = Integer.parseInt(JOptionPane.showInputDialog("int2"));
-		System.out.println(calculator.add());
-		System.out.println(calculator.subtract());
-		System.out.println(calculator.multiply());
-		System.out.println(calculator.divide());
-
+		calculator.createUI();
+		
 	}
 
 }
